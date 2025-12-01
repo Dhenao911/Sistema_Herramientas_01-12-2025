2dcc573b8916e9f13e98145240040da1513e7bb4
Dhenao911 <dhenao911@gmail.com>
2025-12-01 12:59:21 -0500

    Add IEmployeeRepository

---
+++ SH.Backend/Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using SH.Share.Models;
+
+namespace SH.Backend.Repositories.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<ICollection<Employee>> GetEmployeeAsync();
+
+        Task<Employee> GetEmployeeAsync(int id);
+    }
+}
